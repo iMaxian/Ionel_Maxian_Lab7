e159d58b87a4371a2e465a1db0bda3a886af073a
IONEL MAXIAN <ionel.maxian@stud.ubbcluj.ro>
2023-12-07 19:45:27 +0200

    lab9

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -22,16 +22,43 @@ namespace Ionel_Maxian_Lab7.Data
 
         }
 
-        public Task<int> SaveProductAsync(Product product) { if (product.ID != 0) { return _database.UpdateAsync(product); } else { return _database.InsertAsync(product); } }
-        public Task<int> DeleteProductAsync(Product product) { return _database.DeleteAsync(product); }
-        public Task<List<Product>> GetProductsAsync() { return _database.Table<Product>().ToListAsync(); }
+        public Task<int> SaveProductAsync(Product product) 
+        {
+            if (product.ID != 0) 
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
 
-        public Task<int> SaveListProductAsync(ListProduct listp) { if (listp.ID != 0) { return _database.UpdateAsync(listp); } else { return _database.InsertAsync(listp); } }
-        public Task<List<Product>> GetListProductsAsync(int shoplistid) { return _database.QueryAsync<Product>("select P.ID, P.Description from Product P" + " inner join ListProduct LP" + " on P.ID = LP.ProductID where LP.ShopListID = ?", shoplistid); }
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0) 
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else 
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>("select P.ID, P.Description from Product P" + " inner join ListProduct LP" + " on P.ID = LP.ProductID where LP.ShopListID = ?", shoplistid);
+        }
 
-    }
-} 
-        /*
+        
         public Task<List<ShopList>> GetShopListsAsync() 
         { 
             return _database.Table<ShopList>().ToListAsync(); 
@@ -56,4 +83,4 @@ namespace Ionel_Maxian_Lab7.Data
             return _database.DeleteAsync(slist); 
         }
     } 
-}*/
+}
